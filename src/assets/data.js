b7c820d15eab33c7527c72e41dd959bc36b9efc7
MajaZubko <majamzubko@gmail.com>
2022-10-23 15:46:15 +0200

    Create data file with texts to translate

---
+++ src/assets/data.js
@@ -0,0 +1,13 @@
+export default [
+    'dzień dobry', 'dzień dobry (po południu)', 'miłego dnia', 'dobry wieczór', 'dobranoc', 'śpij dobrze', 'do widzenia', 'do zobaczenia', 'do potem',
+    'witajcie', 'jak się masz?', 'co jest?', 'dobrze, a ty?', 'tak sobie', 'niezbyt dobrze', 'całkiem dobrze', 'źle', 'do dupy', 'ja też mam się dobrze',
+    'dziękuję', 'przepraszam', 'przepraszam (przejść)', 'przykro mi', 'proszę', '...proszę', 'nie ma za co', 'nie ma sprawy', 'słucham (telefon)',
+    'jak się nazywasz?', 'nazywam się...', 'miło mi', 'smacznego', 'zdrowie', 'nawzajem', 'pomocy', 'czy mógłbyś...proszę?', 'czy mógłbym prosić cię o przysługę?',
+    'chciałbym poprosić cię o przysługę', 'czy mógłbyś mi pomóc?', 'uwaga', 'jak się mówi...po włosku?', 'co znaczy...?', 'tak', 'nie', 'oczywiście',
+    'dobrze', 'zgoda', 'nie zgadzam się', 'nie masz racji', 'być może', 'nie wiem', 'nie rozumiem', 'nie pamiętam', 'nie martw się', 'spokojnie',
+    'o boże', 'super', 'fajnie', 'zajebiscie', 'idealnie', 'perfekcyjnie', 'chyba żartujesz', 'co mówisz?', 'co robisz?', 'co porabiasz?', 'co masz na myśli?',
+    'mógłbyś powtórzyć?', 'jesteś pewien?', 'wydaje mi się, że...', 'uważam, że...', 'myślę, że...', 'lubię...', 'uwielbiam...', 'kocham...', 'nienawidzę...',
+    'o, tu jest!', 'wiesz co...', 'słuchaj', 'musimy porozmawiać', 'cierpliwości', 'poczekaj', 'chwila!', 'moment!', 'a więc...', 'nieważne',
+    'zostaw ten temat', 'olej to', 'no co ty?', '..., prawda?', 'czy to prawda, że...?', 'naprawdę?', 'serio?', 'nie wierzę', 'przypominam ci, że...',
+    'spójrz tu na chwilę', 'czy masz...?', 'ile masz lat?', 'czy lubisz...?', 'masz ochotę na...?', 'na twoim miejscu...', 'zrób to', 'nie rób tego', 'idź'
+]
