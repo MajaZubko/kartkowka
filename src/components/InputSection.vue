9c8aad397d53112178136b16dad97a3237ca9544
MajaZubko <majamzubko@gmail.com>
2022-10-23 15:46:36 +0200

    Create Input Section component with label

---
+++ src/components/InputSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="input-section">
+    <span class="input-label">{{textToTranslate}}</span>
+    <input />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputSection',
+  props: {
+    textToTranslate: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+.input-section {
+  display: flex;
+  min-width: 40vw;
+  margin-bottom: 8px;
+}
+
+.input-label {
+  min-width: 20vw;
+}
+
+input {
+  min-width: 20vw;
+}
+</style>
