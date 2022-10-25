312b451e7e85351d59957baa3018f1bbaead56df
MajaZubko <majamzubko@gmail.com>
2022-10-25 22:57:56 +0200

    Prepare more texts to translate

--- src/assets/data.js
+++ src/assets/data.js
@@ -9,5 +9,14 @@ export default [
     'mógłbyś powtórzyć?', 'jesteś pewien?', 'wydaje mi się, że...', 'uważam, że...', 'myślę, że...', 'lubię...', 'uwielbiam...', 'kocham...', 'nienawidzę...',
     'o, tu jest!', 'wiesz co...', 'słuchaj', 'musimy porozmawiać', 'cierpliwości', 'poczekaj', 'chwila!', 'moment!', 'a więc...', 'nieważne',
     'zostaw ten temat', 'olej to', 'no co ty?', '..., prawda?', 'czy to prawda, że...?', 'naprawdę?', 'serio?', 'nie wierzę', 'przypominam ci, że...',
-    'spójrz tu na chwilę', 'czy masz...?', 'ile masz lat?', 'czy lubisz...?', 'masz ochotę na...?', 'na twoim miejscu...', 'zrób to', 'nie rób tego', 'idź'
+    'spójrz tu na chwilę', 'czy masz...?', 'ile masz lat?', 'czy lubisz...?', 'masz ochotę na...?', 'na twoim miejscu...', 'zrób to', 'nie rób tego', 'idź',
+    'która godzina?', 'co jemy na obiad?', 'gdzie jest najbliższa kawiarnia?', 'gdzie jest najbliższa pizzeria?', 'jak dojść do...?', 'któregoś razu...',
+    'pierwszy raz...', 'powodzenia!', 'dasz radę! jestem tego pewien', 'mieszkam tu niedaleko', 'dokąd idziesz?', 'to kosztuje...', 'czekam na...',
+    'nudzi mi się', 'jestem umówiony na wizytę', 'ogólnie', 'wyluzuj', 'po pierwsze', 'genialnie!', 'wiem, co masz na myśli', 'pewnie, czemu nie?',
+    'myślę, że to dobry pomysł', 'przepraszam za kłopot', 'nie przeszkadzam?', 'jaka szkoda!', 'nie mogę się doczekać...', 'na przykład...',
+    'pozdrów ode mnie...', 'niechcący', 'możesz to przeliterować?', 'czy ktoś tu mówi po polsku?', 'kiedy otwiera się...?', 'kiedy zamyka się...?',
+    'jaka jest pogoda?', 'chciałbym zarezerwować stolik dla...', 'można zapłacić gotówką?', 'można zapłacić kartą?', 'na wynos', 'z dostawą do domu',
+    'wszystkiego najlepszego!', 'wygodnie ci?', 'to zależy', 'umieram ze śmiechu', 'śmieszy mnie...', 'tracić czas', 'w wolnym czasie', 'powoli',
+    'cisza', 'bądź cicho', 'przesuń się!', 'ostatnio...', 'przepraszam za spóźnienie', 'coś', 'ktoś', 'gdzieś/w jakimś miejscu', 'jakoś/w jakiś sposób',
+    'kiedyś/w jakimś momencie', 'nie wiem jak ty...', 'co mi do tego?', 'od zawsze...', 'za dzieciaka...', 'gratulacje!'
 ]
